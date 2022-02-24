687fe0c35fcaf51b8252c60d9d3e7bec656b41d6
Louis Machin <ldhmachin@gmail.com>
2022-02-24 17:06:48 +0000

    Add prompt commands

--- lib/events/miscellaneous.rb
+++ lib/events/miscellaneous.rb
@@ -12,3 +12,18 @@ end
 $bot.message(start_with: '~lofi') do |event|
     event.respond File.readlines('./data/lofi.txt').sample
 end
+
+$bot.message(start_with: '~prompt') do |event|
+    event.respond format_standard(File.readlines('./data/prompts.txt').sample) if event.content == "~prompt"
+end
+
+$bot.message(start_with: '~prompt-add') do |event|
+    words = event.content.split(" ")
+    if words.length > 1
+        desc = words[1..-1].join(" ")
+        File.open('./data/prompts.txt', 'a') { |f| f.write(desc) }
+        event.respond format_success("Added prompt!")
+    else
+        event.respond format_usage("~prompt-add <description>")
+    end
+end
