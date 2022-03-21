110a563b29e3f38c14091f1a55cb1191f41844ea
Louis Machin <ldhmachin@gmail.com>
2022-03-21 13:28:56 +0000

    Added percentage value to progress bar

--- lib/png.rb
+++ lib/png.rb
@@ -12,12 +12,23 @@ DATA_CLRS = [
     "#b3de69", "#fccde5", "#d9d9d9", "#ffffb3",
 ]
 
+def chunky_text_width(str)
+    width, letter_spacing, letter_width = 0, 2, 0
+    str.chars.each_with_index do |char, position|
+        pixels = FONT.dig("glyphs", char, "pixels")
+        unless pixels == nil
+            width += letter_spacing + pixels.first.length
+        end
+    end
+    return width
+end
+
 def chunky_print(png, x, y, str, clr = chunky_clr("#000000"))
     left, letter_spacing, letter_width = 0, 2, 0
     str.chars.each_with_index do |char, position|
-        letter = FONT["glyphs"][char]
-        if letter.key?("pixels")
-            letter["pixels"].each_with_index do |row, j|
+        pixels = FONT.dig("glyphs", char, "pixels")
+        unless pixels == nil
+            pixels.each_with_index do |row, j|
                 letter_width = row.length
                 row.each_with_index do |state, i|
                     png[x + i + left, y + j] = clr if state == 1
@@ -58,6 +69,10 @@ def bar_graph_png(percentages = [0.3, 0.5, 0.1, 0.7, 0.4])
         png.rect(margin, top, margin + bar_width * percentage, top + bar_height, chunky_clr, chunky_clr("#{DATA_CLRS[index]}AA"))
         png.rect(margin, top, margin + bar_width, top + bar_height, chunky_clr("#505673"))
         png = chunky_print(png, margin + 4, top + 3, caption, chunky_clr("#FFFFFFDD"))
+        # Percentage text
+        caption = (percentage * 100).floor.to_s.rjust(3, " ") + "%"
+        left = width - (1 + margin + chunky_text_width(caption))
+        png = chunky_print(png, left, top + 3, caption, chunky_clr("#FFFFFFDD"))
     end
   # png[1,1] = ChunkyPNG::Color.rgba(10, 20, 128, 128)
   # png[2,1] = ChunkyPNG::Color('black @ 0.5')
