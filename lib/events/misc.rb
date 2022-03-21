581044540f7e5462fb2543a5b97cff9815ca1e24
Louis Machin <ldhmachin@gmail.com>
2022-03-21 14:23:34 +0000

    Added phyllotaxis in misc

--- lib/events/misc.rb
+++ lib/events/misc.rb
@@ -29,3 +29,11 @@ $bot.message(start_with: PREFIX + 'prompt') do |event|
         end
     end
 end
+
+$bot.message(start_with: PREFIX + 'phyllotaxis') do |event|
+    args = event.content.split(" ")
+    return unless args.shift == PREFIX + 'phyllotaxis'
+    alpha, scale = args.length == 2 ? [args[0].to_f, args[1].to_f] : [135.5, 2.3]
+    event.respond "α = #{alpha}; c = #{scale}"
+    event.send_file(File.open(phyllotaxis_png(alpha, scale), 'r'))
+end
--- lib/png.rb
+++ lib/png.rb
@@ -13,12 +13,10 @@ DATA_CLRS = [
 ]
 
 def chunky_text_width(str)
-    width, letter_spacing, letter_width = 0, 2, 0
-    str.chars.each_with_index do |char, position|
+    width, letter_spacing = 0, 2
+    str.chars.each do |char|
         pixels = FONT.dig("glyphs", char, "pixels")
-        unless pixels == nil
-            width += letter_spacing + pixels.first.length
-        end
+        width += letter_spacing + pixels.first.length unless pixels == nil
     end
     return width
 end
@@ -74,9 +72,30 @@ def bar_graph_png(percentages = [0.3, 0.5, 0.1, 0.7, 0.4])
         left = width - (1 + margin + chunky_text_width(caption))
         png = chunky_print(png, left, top + 3, caption, chunky_clr("#FFFFFFDD"))
     end
-  # png[1,1] = ChunkyPNG::Color.rgba(10, 20, 128, 128)
-  # png[2,1] = ChunkyPNG::Color('black @ 0.5')
-  # png.line(1, 1, 100, 80, ChunkyPNG::Color.from_hex('#aa007f'))
+    png.save(filename, :interlace => true)
+    return filename
+end
+
+def phyllotaxis_png(alpha = 135.5, scale = 2.3)
+    def within_bound(x, y)
+        return !((x < 0) or (y < 0) or (x > 399) or (y > 399))
+    end
+    def point(alpha, scale, n, rounded = true)
+        φ = n * alpha * Math::PI / 180
+        r = scale * Math.sqrt(n)
+        x = r * Math.cos(φ) + 200
+        y = r * Math.sin(φ) + 200
+        return rounded ? [x.round, y.round] : [x, y]
+    end
+    # File name
+    filename = "./temp/#{(0...8).map { (65 + rand(26)).chr }.join}.png"
+    width, height = 400, 400
+    png = ChunkyPNG::Image.new(width, height, chunky_clr)
+    # Create list of points
+    points = Array.new(50000) { |n| point(alpha, scale, n) }
+    # Iterate through each point and add to canvas
+    points.each { |x, y| png[x, y] = chunky_clr("#FFFFFF") if within_bound(x, y) }
+    # Return
     png.save(filename, :interlace => true)
     return filename
 end
